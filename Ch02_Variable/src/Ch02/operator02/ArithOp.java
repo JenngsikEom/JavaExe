50524e27e6dbaef88e07e017cb40706275982b1c
NCP08 <navercloudplatform08.gmail.com>
2024-01-24 16:59:55 +0900

    2일차

---
+++ Ch02_Variable/src/Ch02/operator02/ArithOp.java
@@ -0,0 +1,14 @@
+package Ch02.operator02;
+
+public class ArithOp {
+	public static void main(String[] args) {
+		int num0 = 10;
+		int num1 = 3;
+		//사칙연산
+		System.out.println(num0+num1);
+		System.out.println(num0-num1);
+		System.out.println(num0*num1);
+		System.out.println(num0/num1);  // 몫
+		System.out.println(num0%num1);  // 나머지
+	}
+}
